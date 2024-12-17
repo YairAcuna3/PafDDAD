ca6281fca1928ce4fca2a61ddb42fccd5bcf7347
YairAc3 <yairacuna30@gmail.com>
2024-12-16 19:16:32 -0500

    primer commit uwu

---
+++ src/main/java/com/acuna/denunciaservice/EFinalAcunaApplication.java
@@ -0,0 +1,13 @@
+package com.acuna.denunciaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFinalAcunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFinalAcunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acuna/denunciaservice/EFinalAcunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acuna.denunciaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFinalAcunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
